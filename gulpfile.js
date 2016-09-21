f5bbbee1527482027c6d823b1adb8d18867e2d30
Nathan Kessler <nathant93@gmail.com>
2016-09-20 22:23:41 -0400

    Reorder and cleanup gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,47 +2,75 @@ var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var inject      = require('gulp-inject');
 var clean       = require('gulp-clean');
+var series      = require('stream-series');
+var debug       = require('gulp-debug');
 
-gulp.task('serve', ['vendor', 'template', 'inject'], function () {
+gulp.task('serve', ['inject'], function () {
 
-    // Serve files from the root of this project
     browserSync.init({
         server: {
-            baseDir: "./build"
+            baseDir: './build'
         }
     });
 
-    gulp.watch("./app/**/*.html").on("change", browserSync.reload);
-});
-
-
-
-gulp.task('template', function () {
-
-    gulp.src(['./src/**/*.html'])
-        .pipe(gulp.dest('./build'));
-});
-
-gulp.task('vendor', function () {
-
-    gulp.src(['./src/vendor/css/*.css'])
-        .pipe(gulp.dest('./build/vendor/css'));
-
-    gulp.src(['./src/vendor/js/*.js'])
-        .pipe(gulp.dest('./build/vendor/js'));
-});
-
-gulp.task('inject', ['template', 'vendor'], function () {
-    var target = gulp.src('./build/index.html');
-    var vendorSources = gulp.src(
-        ['./build/vendor/js/**/*.js', './build/vendor/css/**/*.css'],
-        {read: false});
-
-    return target.pipe(inject(vendorSources))
-        .pipe(gulp.dest('./build'));
+    gulp.watch('./app/**/*.html').on('change', browserSync.reload);
 });
 
 gulp.task('clean', function () {
     return gulp.src('./build', {read: false})
         .pipe(clean());
 });
+
+/* Copy HTML to build*/
+gulp.task('template', function () {
+
+    return gulp.src(['./src/**/*.html'])
+        .pipe(gulp.dest('./build'));
+});
+
+/* Vendor CSS and Javascript */
+gulp.task('vendor-resources', ['vendor-css', 'vendor-js'], function () {
+    return true;
+});
+
+gulp.task('vendor-js', function () {
+    return gulp.src(['./src/vendor/js/*.js'])
+        .pipe(gulp.dest('./build/vendor/js'));
+});
+
+gulp.task('vendor-css', function () {
+    return gulp.src(['./src/vendor/css/**/*.css'])
+        .pipe(gulp.dest('./build/vendor/css'));
+});
+
+/* User CSS and JS into build directory  */
+gulp.task('user-resources', ['user-css', 'user-js'], function () {
+    return true;
+});
+
+gulp.task('user-js', function () {
+    return gulp.src(['./src/js/**/*.js'])
+        .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('user-css', function () {
+    return gulp.src(['./src/css/**/*.css'])
+        .pipe(gulp.dest('./build/css'));
+});
+
+/* Inject CSS and JS tags into index.html */
+gulp.task('inject',
+    ['vendor-resources', 'user-resources', 'template'],
+    function () {
+
+        var vendorSources = gulp.src(
+            ['./build/vendor/js/**/*.js', './build/vendor/css/**/*.css'],
+            {read: false});
+        var userSources = gulp.src (
+            ['./build/js/**/*.js', './build/css/**/*.css'],
+            {read: false});
+
+        return gulp.src('./build/index.html')
+            .pipe(inject(series(vendorSources, userSources), {relative: true}))
+            .pipe(gulp.dest('./build'));
+    });
