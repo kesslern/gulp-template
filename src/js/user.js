dcc69c1ef78c898691e848f06a2193576f8fca3d
Nathan Kessler <nathant93@gmail.com>
2016-09-25 21:33:30 -0400

    Add angular routing

--- src/js/user.js
+++ src/js/user.js
@@ -1,5 +1,26 @@
-angular.module("mainApp", [])
+angular.module("mainApp", ['ui.router'])
     .controller('mainController', ['$scope', function($scope) {
 
-        $scope.header = "Starter Template"
-    }]);
+        $scope.header = "Starter Template";
+    }])
+    .config(function($stateProvider) {
+        $stateProvider
+            .state('index', {
+            url: '/',
+            templateUrl: 'starter-template.html'
+            })
+            .state('about', {
+                url: '/about',
+                templateUrl: 'about/about.html'
+            });
+    });
+
+
+// TODO: Use a directive
+(function($){
+    $(function(){
+
+        $('.button-collapse').sideNav();
+
+    }); // end of document ready
+})(jQuery); // end of jQuery name space
